05d29b137106e8b3edf0ccb945185434dab4bba3
ethano1323 <ethano1323@yahoo.com>
2025-12-19 16:05:35 -0600

    Update app.py

--- Inputs/app.py
+++ Inputs/app.py
@@ -282,7 +282,7 @@ st.subheader("Stat Definitions")
 st.markdown(
     """    
     **Opponent:** Matchup for the week  
-    **Route Share:** Percentage of the team's total dropbacks that this player ran a route on 
+    **Route Share:** Percentage of the team's total dropbacks that this player ran a route on
     **Base YPRR:** Player's base yards per route run this season  
     **Adjusted YPRR:** Projected YPRR based on opponent's typical coverage, safety looks, and blitz rates
     **Edge:** Percentage difference between Adjusted YPRR and Base YPRR, including a sample size penalty (accounts for limited route-share and other factors)  
